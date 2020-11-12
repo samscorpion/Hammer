93c4dfd256c07f75eb9d09541ef6331f87bd391b
Samuel <samscopion@gmail.com>
2020-11-11 21:49:56 -0500

    prueba terminada

--- app.js
+++ app.js
@@ -1,15 +1,49 @@
-const cuerpo = document.querySelector(".cuerpo");
+const cuerpo = document.querySelector(".contenido");
+const nodos = document.querySelectorAll(".cuerpo")
 
 const hammertime = new Hammer(cuerpo);
+let contador = 0;
+hammertime.get('swipe').set({ direction: Hammer.DIRECTION_HORIZONTAL });
 
-hammertime.on("pan ",function(e){
-    console.log(e);
-    e.preventDefault();
-    cuerpo.textContent = e.type;
+// hammertime.on("pan",function(e){
+//     console.log(e);
+//     e.preventDefault();
+//     cuerpo.textContent = e.type;
+// })
+
+hammertime.on("swipeleft",function(e){
+    
+    if(nodos.length-1 > contador){
+        contador+=1;
+        nodos[contador-1].classList.remove("active")
+        nodos[contador].classList.add("active");
+        if(contador == 1){
+            nodos[contador].style.background = "lime";
+        }
+        if(contador == 2){
+            nodos[contador].style.background = "aqua";
+       }
+    }
+    console.log(contador);
 })
 
-hammertime.on("swipe",function(e){
-    console.log(e);
-    cuerpo.style.background = "red";
-    cuerpo.textContent = e.type;
+hammertime.on("swiperight",function(e){
+    if(contador > 0){
+        contador-=1;
+        nodos[contador+1].classList.remove("active")
+        nodos[contador].classList.add("active")
+        if(contador == 1){
+            nodos[contador].style.background = "lime";
+        }
+        if(contador == 2){
+            nodos[contador].style.background = "aqua";
+       }
+       
+    }
+    console.log(contador);
+    // console.log(e.type);
+    // cuerpo.style.background = "green";
+    // // cuerpo.textContent = e.type;
+    // cuerpo.firstChild.nextSibling;
 })
+console.log(nodos.length)
